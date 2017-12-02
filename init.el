ce08d872d357cfa92ff6a55a3ef5f9adc341075b
wp-mac <weipan789@gmail.com>
2017-12-02 17:32:28 +0800

    as my main config emacs

--- init.el
+++ init.el
@@ -9,6 +9,12 @@
   ;;  (add-to-list 'package-archives '("melpas" . "https://melpa.org/packages/") t)
   (package-initialize))
 
+(setq idea-projects "~/IdeaProjects/")
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+;;使用最新的org-mode  git clone git://orgmode.org/org-mode.git
+(add-to-list 'load-path (expand-file-name " org-mode/lisp" idea-projects))
+(add-to-list 'load-path (expand-file-name " org-mode/contrib/lisp" idea-projects))
+
 
 (when (not package-archive-contents)
   (package-refresh-contents))
@@ -21,7 +27,7 @@
 
 
 (setq emacs-load-start-time (current-time))
-(setq user-emacs-directory "~/.emacs.dev.d/")
+(setq user-emacs-directory "~/.emacs.d/")
 ;;添加定制的el
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
@@ -32,6 +38,12 @@
 (add-to-list 'load-path "~/Documents/emacs-plugins/yasnippet")
 
 
+(setq my-config "/home/wp/.emacs.d/my-config.el")
+(when (file-exists-p my-config)
+  (load my-config))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 
 (require 'setup-general)
 (if (version< emacs-version "24.4")
@@ -40,7 +52,7 @@
   (require 'setup-helm-gtags))
 ;; (require 'setup-ggtags)
 (require 'setup-cedet)
-(require 'setup-editing)
+;;(require 'setup-editing)
 
 
 
@@ -54,8 +66,8 @@
   '(paredit clojure-mode clojure-mode-extra-font-locking cider
     ido-ubiquitous smex
     rainbow-delimiters tagedit
-    magit
-    dired+
+    ;;magit
+    ;;dired+
     auto-complete
     ace-jump-mode
     ecb
@@ -87,9 +99,4 @@
 ;;(require 'yasnippet)
 ;;(yas-global-mode 1)
 
-
-;;----------------------------------------------------------------------------
-;; Variables configured via the interactive 'customize' interface
-;;----------------------------------------------------------------------------
-(when (file-exists-p custom-file)
-  (load custom-file))
+(provide 'init)
--- lisp/init-setting.el
+++ lisp/init-setting.el
@@ -1,9 +1,9 @@
 (tool-bar-mode 0)  
 (menu-bar-mode 0)  
-(scroll-bar-mode 0)
+;;(scroll-bar-mode 0)
 
 
-(load-theme 'wombat t)
-(set-default-font "-PfEd-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+;;(load-theme 'wombat t)
+;;(set-default-font "-PfEd-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
 
 (provide 'init-setting)
