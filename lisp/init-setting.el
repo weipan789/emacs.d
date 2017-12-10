0866bc222073c25cb65a031f5e4d2ac171baedc6
wp <weipan789@gmail.com>
2017-12-10 15:44:25 +0800

    绑定快捷键到magit

--- lisp/init-setting.el
+++ lisp/init-setting.el
@@ -6,4 +6,7 @@
 ;;(load-theme 'wombat t)
 ;;(set-default-font "-PfEd-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
 
+
+;;绑定快捷键到magit
+(global-set-key (kbd "C-x g") 'magit-status)
 (provide 'init-setting)
